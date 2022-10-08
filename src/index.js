8c01a7ff26978c59a6adeb6886c4ab35c472aeae
eyupGuc <meydanguresi@gmail.com>
2022-10-08 12:21:59 +0300

    class render

--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+// import App from './App';
+const App=()=>{
+    return (<> <h1>Hello World</h1>
+    <p>Merhaba</p> <Child/></>
+    )
+}
 
+const Child=()=><p>I am child</p>
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+root.render(<App/>
  
-    <App />
+    // <App />
   
 );
 
